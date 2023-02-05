44970fed8169c7bb548de073a3c45a0dd662a41f
BeatrizBravo <123078841+BeatrizBravo@users.noreply.github.com>
2023-02-05 14:47:19 +0000

    4 pages of html, 1 js and 1 css

---
+++ page.js
@@ -0,0 +1,15 @@
+
+setTimeout(person,300)
+let person = prompt("What is your name?", "Friend");
+document.getElementById("text-from-promt").innerHTML = "Welcome to  " + person + "!!";
+
+
+s
+
+$("#button").click(function(){
+    $(".curry").fadeIn("slow");
+    $("#button").attr('id', 'clicked')
+});
+
+
+
